c56528c7da8908e9ad379d4da41273883a4a6842
İlkan <ilkansilcan3210@gmail.com>
2025-04-30 22:30:52 +0300

    OdevCozumLenna.py güncellendi

--- OdevCozumLenna.py
+++ OdevCozumLenna.py
@@ -3,24 +3,41 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-img = cv2.imread('D:/Final_BonusOdevGoruntuleri/Lenna.jpg', 0)
+img = cv2.imread('D:/Final_BonusOdevGoruntuleri/Lenna.jpg', cv2.IMREAD_GRAYSCALE)
+
+
 f = np.fft.fft2(img)
 fshift = np.fft.fftshift(f)
+
+
+magnitude_spectrum = 20 * np.log(np.abs(fshift) + 1)  # +1 ile log(0) hatası önlenir
 
 
 rows, cols = img.shape
 crow, ccol = rows // 2, cols // 2
 mask = np.zeros((rows, cols), np.uint8)
-mask[crow-30:crow+30, ccol-30:ccol+30] = 1
+mask_size = 30
+mask[crow - mask_size:crow + mask_size, ccol - mask_size:ccol + mask_size] = 1
 
-fshift = fshift * mask
-f_ishift = np.fft.ifftshift(fshift)
+
+fshift_filtered = fshift * mask
+
+
+f_ishift = np.fft.ifftshift(fshift_filtered)
 img_back = np.fft.ifft2(f_ishift)
 img_back = np.abs(img_back)
 
 
-plt.subplot(121), plt.imshow(img, cmap='gray')
+plt.figure(figsize=(12, 8))
+
+plt.subplot(131), plt.imshow(img, cmap='gray')
 plt.title('Orijinal Görüntü'), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(img_back, cmap='gray')
-plt.title('Gürültüsü Azaltılmış Görüntü'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(132), plt.imshow(magnitude_spectrum, cmap='gray')
+plt.title('Frekans Spektrumu'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(133), plt.imshow(img_back, cmap='gray')
+plt.title('Düşük Frekanslı Filtrelenmiş'), plt.xticks([]), plt.yticks([])
+
+plt.tight_layout()
 plt.show()
