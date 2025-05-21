7085307a1200253ae3f8368f5f76b76aa7067dc2
İlkan <ilkansilcan3210@gmail.com>
2025-05-21 23:17:38 +0300

    hesap_makinesi_c++.cc yeni özellikler eklendi ve dosya güncellendi

--- hesap_makinesi_c++.cc
+++ hesap_makinesi_c++.cc
@@ -1,37 +1,67 @@
 #include <iostream>
+#include <limits> // numeric_limits kullanımı için
 using namespace std;
 
 int main() {
     double num1, num2;
     char operation;
+    char again;
 
-    cout << "Birinci sayiyi girin: ";
-    cin >> num1;
+    cout << "Basit Hesap Makinesi (Cikis icin 'q' girin)\n";
 
-    cout << "Yapmak istediginiz islemi girin (+, -, *, /): ";
-    cin >> operation;
+    while (true) {
+        cout << "\nBirinci sayiyi girin: ";
+        if (!(cin >> num1)) {
+            cout << "Gecersiz sayi girdiniz. Program sonlandiriliyor." << endl;
+            break;
+        }
 
-    cout << "Ikinci sayiyi girin: ";
-    cin >> num2;
+        cout << "Yapmak istediginiz islemi girin (+, -, *, /): ";
+        cin >> operation;
 
-    switch (operation) {
-        case '+':
-            cout << "Sonuc: " << num1 + num2 << endl;
+        if (operation == 'q') {
+            cout << "Programdan cikiliyor..." << endl;
             break;
-        case '-':
-            cout << "Sonuc: " << num1 - num2 << endl;
+        }
+
+        cout << "Ikinci sayiyi girin: ";
+        if (!(cin >> num2)) {
+            cout << "Gecersiz sayi girdiniz. Program sonlandiriliyor." << endl;
             break;
-        case '*':
-            cout << "Sonuc: " << num1 * num2 << endl;
+        }
+
+        switch (operation) {
+            case '+':
+                cout << "Sonuc: " << num1 + num2 << endl;
+                break;
+            case '-':
+                cout << "Sonuc: " << num1 - num2 << endl;
+                break;
+            case '*':
+                cout << "Sonuc: " << num1 * num2 << endl;
+                break;
+            case '/':
+                if (num2 != 0)
+                    cout << "Sonuc: " << num1 / num2 << endl;
+                else
+                    cout << "Hata: Sifira bolme yapilamaz!" << endl;
+                break;
+            default:
+                cout << "Gecersiz islem girdiniz!" << endl;
+        }
+
+        // Kullanıcı tekrar işlem yapmak istiyor mu?
+        cout << "\nBaska bir islem yapmak istiyor musunuz? (e/h): ";
+        cin >> again;
+
+        if (again == 'h' || again == 'H') {
+            cout << "Programdan cikiliyor..." << endl;
             break;
-        case '/':
-            if (num2 != 0)
-                cout << "Sonuc: " << num1 / num2 << endl;
-            else
-                cout << "Sifira bolme hatasi!" << endl;
-            break;
-        default:
-            cout << "Gecersiz islem!" << endl;
+        }
+
+        // Hatalı giriş sonrası input temizliği
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
     return 0;
